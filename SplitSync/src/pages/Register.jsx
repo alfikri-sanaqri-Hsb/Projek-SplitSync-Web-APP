18d4a0d280015a8d8cefd9567348926b46902daa
Alfikri <alfikrisanaqri@student.ub.ac.id>
2026-05-11 02:13:51 +0700

    melanjutkan progres website

--- SplitSync/src/pages/Register.jsx
+++ SplitSync/src/pages/Register.jsx
@@ -5,7 +5,7 @@ import Logo from "@/assets/logo.png";
 export default function Register() {
   return (
     <MainLayout>
-      <div className="flex items-center justify-center min-h-[70vh] px-4 mt-12">
+      <div className="flex items-center justify-center min-h-[70vh] px-4 my-20">
         <div className="w-full max-w-md bg-gray-200 dark:bg-gray-900 p-8 rounded-2xl shadow-xl">
           
           <div className="flex justify-center mb-4">
@@ -18,7 +18,7 @@ export default function Register() {
           <h2 className="text-black dark:text-white text-2xl font-bold mb-2 text-center">
             Create Account
           </h2>
-          <p className="text-sm md:text-base text-gray-600 dark:text-gray-400 mb-12 text-center">
+          <p className="text-sm md:text-base text-gray-600 dark:text-gray-400 mb-5 text-center">
             Register to start splitting bills
           </p>
 
@@ -58,6 +58,25 @@ export default function Register() {
                 type="password"
                 placeholder="********"
                 className="w-full outline-none text-gray-700 bg-transparent"/>
+            </div>
+
+            {/* QRIS UPLOAD (OPTIONAL) */}
+            <div className="mb-6">
+                <label className="text-black dark:text-white text-sm">
+                    Upload QRIS <span className="text-gray-500">(Optional)</span>
+                </label>
+
+            <div className="mt-1">
+                <input
+                type="file"
+                accept="image/*"
+                className="w-full bg-white dark:bg-gray-800 text-gray-700 dark:text-white border border-gray-300 dark:border-gray-700 rounded-lg px-3 py-2 cursor-pointer file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:bg-blue-600 file:text-white hover:file:bg-blue-500"
+                />
+            </div>
+
+                <p className="text-xs text-gray-500 mt-2">
+                    Upload QRIS image for easier payments.
+                </p>
             </div>
           </div>
 
